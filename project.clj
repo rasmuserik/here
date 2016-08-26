d98d59a0a0fdac63ceb8de4919e82ae4078b902d
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-08-26 07:23:12 +0200

    initial commit

---
+++ project.clj
@@ -0,0 +1,59 @@
+(defproject solsort.here/here "0.0.1-SNAPSHOT"
+
+  :dependencies
+  [[org.clojure/clojure "1.8.0"]
+   [org.clojure/clojurescript "1.9.225"]
+   [org.clojure/core.async "0.2.374"]
+   [cljsjs/localforage "1.3.1-0"]
+   [com.cognitect/transit-cljs "0.8.237"]
+   [reagent "0.6.0-rc"]
+   [com.rpl/specter "0.11.2"]
+   [binaryage/devtools "0.6.1"]]
+
+  :plugins
+  [[lein-cljsbuild "1.1.3"]
+   [lein-ancient "0.6.8"]
+   [lein-figwheel "0.5.4-7"]
+   [lein-bikeshed "0.3.0"]
+   [lein-kibit "0.1.2"]]
+
+  :source-paths ["src/" "util/src/"]
+
+  :profiles
+  {:dev
+   {:dependencies [
+                   [figwheel-sidecar "0.5.4-3"]
+                   [com.cemerick/piggieback "0.2.1"]]
+    :plugins      [[lein-figwheel "0.5.4-3"]
+                   [cider/cider-nrepl "0.13.0-SNAPSHOT"]]
+    }}
+
+  :clean-targets ^{:protect false}
+  ["resources/public/out"
+   "resources/public/index.js"
+   "resources/public/tests.js"
+   "resources/public/out-tests"
+   "figwheel_server.log"
+   "out/"
+   "target/"]
+
+  :cljsbuild
+  {:builds
+   [{:id "dev"
+     :source-paths ["src/" "util/src/"]
+     :figwheel
+     {:websocket-host :js-client-host }
+     :compiler
+     {:main solsort.here
+      :asset-path "out"
+      :output-to "resources/public/index.js"
+      :output-dir "resources/public/out"
+      :source-map-timestamp true }}
+    {:id "dist"
+     :source-paths ["src" "util/src/"]
+     :compiler
+     {:output-to "index.js"
+      :main solsort.here
+      :externs ["externs.js"]
+      :optimizations :advanced
+      :pretty-print false}}]})
